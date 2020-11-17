a96802f776e07317827a3cc65abfbc45cadf0ded
Varx <varx@Varxs-Mac-mini.local>
2020-11-17 14:15:48 +0700

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,19 @@
+import '../../node_modules/@splidejs/splide/dist/css/splide.min.css';
 import '../styles/index.scss';
+
+// import mainImage from '../assets/*.png';
+
+
+
+import Splide from '@splidejs/splide';
+new Splide('#splide', {
+  type: 'loop',
+  perPage: 2,
+}).mount();
 
 if (process.env.NODE_ENV === 'development') {
   require('../index.html');
+  require('../test.html');
 }
 
 console.log('webpack starterkit');
